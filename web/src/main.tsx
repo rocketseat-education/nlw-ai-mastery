3e191059f579836c84e49535ef8b1d4eefe64453
victor.malheiro <victor.malheiro@compasso.com.br>
2023-09-14 11:40:06 -0300

    fix: (main) change app path

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './app'
+import { App } from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
